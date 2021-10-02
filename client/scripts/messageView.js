e06b70d05d9b4406a5a6378bd4606d0636ac0600
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-10-02 13:42:13 -0400

    Implemented MessageView.render()

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -5,13 +5,31 @@ var MessageView = {
   // Learn more about Underscore's templating capability
   // here: https://underscorejs.org/#template.
   // TODO: Update this template accordingly.
+  //_.template returns a function. that means render is itself a function
+  //render will take an input of a message object (called in messagesView.renderMessages)
   render: _.template(`
-      <!--
+
       <div class="chat">
-        <div class="username"></div>
-        <div></div>
+        <div class="username"> <%= username %> </div>
+        <div> <%= text %> </div>
       </div>
-      -->
+
     `)
 
 };
+
+//template syntax:    <%= [object key] %>
+
+/*
+
+campus: "hr-sfo"
+created_at: "2021-10-02T17:37:08.498Z"
+github_handle: "AdamPJohnson"
+message_id: 36290
+roomname: "heavens drip"
+text: "That was me for some reason I was back on anonymous :/"
+updated_at: "2021-10-02T17:37:08.498Z"
+username: "David Deaton"
+
+
+*/
