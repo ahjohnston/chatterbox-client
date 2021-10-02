490da0862b048e524fabc49b65493eb5b044a9d9
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-10-02 12:29:10 -0400

    Implemented Messages.update and Messages.retrieve

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -5,9 +5,23 @@
 var Messages = {
 
   // TODO: Define how you want to store your messages.
-  _data: null,
+  _data: [],
 
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
+
+  update: (data) => {
+    this._data = data;
+    console.log('data stored in Messages._data is now: ', this._data);
+  },
+  //get a list of all message
+  //get a list of messages fora particular user
+  retrieve: (friend) => {
+    if (!friend) {
+      return this._data;
+    }
+    //if a friend IS passed in, return only messages that were authored by that friend
+
+  }
 
 };
